225dcd4dacfe1f85702e3af345ccfa25dfb68c25
Jarrod Millman <jarrod.millman@gmail.com>
2020-08-22 14:57:34 -0700

    Add 2.5 docs

---
+++ networkx-2.4/inserttext.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+file_name = "index.html"
+
+paths = Path('.').glob('**/*.html')
+for path in paths:
+    file_name = str(path)
+    print(file_name)
+
+    with open(file_name) as in_file:
+        old_content = in_file.readlines()
+    
+    with open(file_name, "w") as in_file:
+        for line in old_content:
+            in_file.write(line)
+            if "wy-side-nav-search" in line:
+                 in_file.write('\n    <a href="https://networkx.github.io/">Project Homepage</a> |')
+                 in_file.write('\n    <a href="https://github.com/networkx/networkx">Source Code</a>\n')
---
+++ networkx-2.4/inserttext2.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+file_name = "index.html"
+
+paths = Path('.').glob('**/*.html')
+for path in paths:
+    file_name = str(path)
+    print(file_name)
+
+    with open(file_name) as in_file:
+        old_content = in_file.readlines()
+    
+    with open(file_name, "w") as in_file:
+        for line in old_content:
+            in_file.write(line)
+            if 'role="main" class="document"' in line:
+                 in_file.write('\n<div class="admonition warning">')
+                 in_file.write('\n<p class="admonition-title">Warning</p>')
+                 in_file.write('\n  <p>This documents an unmaintained version of NetworkX. Please upgrade to a maintained version and see <a href="https://networkx.github.io/documentation/stable/">the current NetworkX documentation</a>.</p>')
+                 in_file.write('\n</div>\n')
